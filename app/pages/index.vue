e1e4980bce0c125a705c68c9573450a5a4a6a40a
selinafehn <144441353+selinafehn@users.noreply.github.com>
2025-03-24 14:26:06 +0100

    Added main content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,9 +3,178 @@
 </script>
 
 <template>
-  <h1> Willkommen auf der Main Seite</h1>
-  <UButton icon="i-lucide-rocket" size="md" color="primary" variant="solid">Button</UButton>
+  <div class="max-w-3xl mx-auto p-8 text-center">
+    <!-- Header Section -->
+    <div>
+      <h1 class="text-5xl font-bold">Hi, I am Selina</h1>
+      <h3 class="text-2xl mt-2">
+        I am a student for computer science and artificial Intelligence
+      </h3>
+    </div>
+
+    <!-- Bio -->
+    <div class="mt-6">
+      <p class="text-lg">🌱 I’m currently studying at university and coding at work</p>
+    </div>
+
+    <!-- Connect Section -->
+    <div class="mt-10">
+      <h3 class="text-xl font-semibold mb-4">Connect with me:</h3>
+      <div class="flex justify-center items-center space-x-4">
+        <div class="flex flex-col items-center">
+          <a
+              href="https://linkedin.com/in/selina fehn"
+              target="_blank"
+              rel="noreferrer"
+          >
+            <img
+                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
+                alt="LinkedIn"
+                class="h-8 w-auto"
+            />
+          </a>
+          <span class="mt-1 text-sm">LinkedIn</span>
+        </div>
+        <!-- Weitere Social Icons können hier analog hinzugefügt werden -->
+      </div>
+    </div>
+
+    <!-- Languages and Tools Section -->
+    <div class="mt-10">
+      <h3 class="text-xl font-semibold mb-4">Languages and Tools:</h3>
+      <div class="flex flex-wrap justify-center items-center gap-8">
+        <!-- Bash -->
+        <div class="flex flex-col items-center">
+          <a href="https://www.gnu.org/software/bash/" target="_blank" rel="noreferrer">
+            <img src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg" alt="Bash" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Bash</span>
+        </div>
+        <!-- Docker -->
+        <div class="flex flex-col items-center">
+          <a href="https://www.docker.com/" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="Docker" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Docker</span>
+        </div>
+        <!-- Figma -->
+        <div class="flex flex-col items-center">
+          <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
+            <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="Figma" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Figma</span>
+        </div>
+        <!-- Git -->
+        <div class="flex flex-col items-center">
+          <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+            <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="Git" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Git</span>
+        </div>
+        <!-- Heroku -->
+        <div class="flex flex-col items-center">
+          <a href="https://heroku.com" target="_blank" rel="noreferrer">
+            <img src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" alt="Heroku" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Heroku</span>
+        </div>
+        <!-- Java -->
+        <div class="flex flex-col items-center">
+          <a href="https://www.java.com" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="Java" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Java</span>
+        </div>
+        <!-- JavaScript -->
+        <div class="flex flex-col items-center">
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="JavaScript" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">JavaScript</span>
+        </div>
+        <!-- Linux -->
+        <div class="flex flex-col items-center">
+          <a href="https://www.linux.org/" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="Linux" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Linux</span>
+        </div>
+        <!-- MongoDB -->
+        <div class="flex flex-col items-center">
+          <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="MongoDB" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">MongoDB</span>
+        </div>
+        <!-- MySQL -->
+        <div class="flex flex-col items-center">
+          <a href="https://www.mysql.com/" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="MySQL" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">MySQL</span>
+        </div>
+        <!-- Node.js -->
+        <div class="flex flex-col items-center">
+          <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="Node.js" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Node.js</span>
+        </div>
+        <!-- Nuxt.js -->
+        <div class="flex flex-col items-center">
+          <a href="https://nuxtjs.org/" target="_blank" rel="noreferrer">
+            <img src="https://www.vectorlogo.zone/logos/nuxtjs/nuxtjs-icon.svg" alt="Nuxt.js" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Nuxt.js</span>
+        </div>
+        <!-- Pandas -->
+        <div class="flex flex-col items-center">
+          <a href="https://pandas.pydata.org/" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/pandas/pandas-original.svg" alt="Pandas" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Pandas</span>
+        </div>
+        <!-- Python -->
+        <div class="flex flex-col items-center">
+          <a href="https://www.python.org" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="Python" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Python</span>
+        </div>
+        <!-- Scala -->
+        <div class="flex flex-col items-center">
+          <a href="https://www.scala-lang.org" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/scala/scala-original.svg" alt="Scala" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Scala</span>
+        </div>
+        <!-- Tailwind CSS -->
+        <div class="flex flex-col items-center">
+          <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+            <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="Tailwind CSS" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Tailwind CSS</span>
+        </div>
+        <!-- TypeScript -->
+        <div class="flex flex-col items-center">
+          <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="TypeScript" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">TypeScript</span>
+        </div>
+        <!-- Vue.js -->
+        <div class="flex flex-col items-center">
+          <a href="https://vuejs.org/" target="_blank" rel="noreferrer">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg" alt="Vue.js" class="h-10 w-auto" />
+          </a>
+          <span class="mt-1 text-sm">Vue.js</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+
 
 <style scoped>
 
